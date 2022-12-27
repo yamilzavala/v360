1bc1ea2d9257a3af365cae0f58bf13b161858112
yamilzavala <zavalayamil9@gmail.com>
2022-12-27 11:02:13 -0300

    first commit

---
+++ src/app/components/graph-container/graph-container.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-graph-container',
+  templateUrl: './graph-container.component.html',
+  styleUrls: ['./graph-container.component.scss']
+})
+export class GraphContainerComponent {
+
+}
